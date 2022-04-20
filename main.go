19bc1c4ed945ed4c5a2cb1a70a7731244cd12c42
czambrano92 <cesar.zambrano.moreno@gmail.com>
2022-04-20 07:35:02 -0400

    first query

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 type Result struct {
 	ID   int
 	Name string
-	Age  int
+	Age  string
 }
 
 func main() {
 
-	f := db.Database()
-	fmt.Println("ffff >> ", f)
+	var result Result
+	db.Database().Raw("SELECT TOP (1000) [ALC_ANO] as ID ,[ALC_SEM] as Name,[ALC_CLASE] as Age FROM [patentes].[dbo].[PC_ALCOHOL] ").Scan(&result)
+
+	fmt.Println("ffff >> ", result)
 
 }
